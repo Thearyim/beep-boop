cbdbd61e33e225a3a5700d50b2876d191b51df1b
ThearyIm <thearyim@gmail.com>
2019-01-25 15:58:09 -0800

    Add if else statement.

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,52 @@
 //  for (var i = 0; i <= inputNumber; i++)
 //   numberArray.push(i);
 // }
-
 function toRange(inputNumber) {
   var range = [];
   for (var num = 0; num <= inputNumber; num++) {
     range += num;
   }
-
   return range;
 }
+
+function beepBoopMachine(range) {
+  var machineOutput;
+  var exception1 = ["0"];
+  var exception2 = ["1"];
+  var exception3 =  ["3"];
+
+  if (exception1.includes(range)) {
+    machineOutput = number + "Beep!";
+  }
+  else if (exception2.includes(range)) {
+    machineOutput = number + "Boop!";
+  }
+  else if (exception3.includes(range)) {
+    machineOutput = number + "I'm sorry, Dave. I'm afraid I can't do that.";
+  }
+
+}
+
+// function toRange(inputNumber) {
+//   var range = [];
+//   for (var num = 0; num <= inputNumber; num++) {
+//     if ()
+//     range += num;
+//   }
+//
+//   return range;
+// }
+
 $(document).ready(function() {
   $("form#beepBoopForm").submit(function(event) {
     event.preventDefault();
     var inputNumber = $("#userInput").val();
+    var numberRangeArray = [];
+    inputNumber.forEach(function(inputNumber) {
+      numberRangeArray.push(beepBoopMachine(inputNumber));
+    });
+    console.log(numberRangeArray);
 
-     var numberRangeArray = [];
-      inputNumber.toRange(function(inputNumber)) {
-        numberRangeArray();
-      }
-      
     // var inputNumberArray = [];
     // for (var i = 0; i <= N; i++) {
     //   inputNumberArray.push(i);
